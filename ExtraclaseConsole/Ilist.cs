382b3f58ae50fab2bdaa3adf9f21f6a4888ae68a
GabrielSL24 <g.soto.1@estudiantec.cr>
2024-09-08 00:22:08 -0600

    Second commit

---
+++ ExtraclaseConsole/Ilist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraclaseConsole2
+{
+    public enum SortDirection { Asc, Desc};
+    public interface Ilist
+    {
+        void InsertInOrder(int value);
+        int DeleteFirst();
+        int DeleteLast();
+        bool DeleteValue(int value);
+        int GetMiddle();
+        void MergeSorted(Ilist listaA, Ilist listaB, SortDirection direction);
+    }
+}
